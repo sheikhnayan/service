cd59557b6bd02b57f866c957db0ca5ebad7401bf
sheikhnayan <sheikhnayan1997@gmail.com>
2023-07-28 06:00:55 +0600

    emial

--- resources/views/auth/reset-password.blade.php
+++ resources/views/auth/reset-password.blade.php
@@ -1,48 +1,231 @@
-<x-guest-layout>
-    <x-auth-card>
-        <x-slot name="logo">
-            <a href="/">
-                <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
-            </a>
-        </x-slot>
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
 
-        <!-- Validation Errors -->
-        <x-auth-validation-errors class="mb-4" :errors="$errors" />
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
 
-        <form method="POST" action="{{ route('password.update') }}">
-            @csrf
+    <title>Transcending Black Excellence</title>
 
-            <!-- Password Reset Token -->
-            <input type="hidden" name="token" value="{{ $request->route('token') }}">
+    <!-- GOOGLE FONTS -->
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:400,500|Poppins:400,500,600,700|Roboto:400,500"
+        rel="stylesheet" />
+    <link href="https://cdn.materialdesignicons.com/3.0.39/css/materialdesignicons.min.css" rel="stylesheet" />
 
-            <!-- Email Address -->
-            <div>
-                <x-label for="email" :value="__('Email')" />
+    <!-- PLUGINS CSS STYLE -->
+    <link href="{{ asset('vendor_panel/assets/plugins/toaster/toastr.min.css') }}" rel="stylesheet" />
+    <link href="{{ asset('vendor_panel/assets/plugins/nprogress/nprogress.css') }}" rel="stylesheet" />
+    <link href="{{ asset('vendor_panel/assets/plugins/flag-icons/css/flag-icon.min.css') }}" rel="stylesheet" />
+    <link href="{{ asset('vendor_panel/assets/plugins/jvectormap/jquery-jvectormap-2.0.3.css') }}" rel="stylesheet" />
+    <link href="{{ asset('vendor_panel/assets/plugins/ladda/ladda.min.css') }}" rel="stylesheet" />
+    <link href="{{ asset('vendor_panel/assets/plugins/select2/css/select2.min.css') }}" rel="stylesheet" />
+    <link href="{{ asset('vendor_panel/assets/plugins/daterangepicker/daterangepicker.css') }}" rel="stylesheet" />
 
-                <x-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email', $request->email)" required autofocus />
+    <!-- SLEEK CSS -->
+    <link id="sleek-css" rel="stylesheet" href="{{ asset('vendor_panel/assets/css/sleek.css') }}" />
+    <link rel="stylesheet" href="{{ asset('vendor_panel/assets/css/custom.css') }}" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/intl-tel-input/12.1.6/css/intlTelInput.css">
+
+
+    <!-- FAVICON -->
+    <link href="{{ asset('vendor_panel/fav.png') }}" rel="shortcut icon" />
+    <!--
+    HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries
+  -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+  <![endif]-->
+    <script src="{{ asset('vendor_panel/assets/plugins/nprogress/nprogress.js') }}"></script>
+    <script src="https://kit.fontawesome.com/efec89e16a.js" crossorigin="anonymous"></script>
+</head>
+
+
+<body class="sidebar-fixed sidebar-dark header-light header-fixed" id="body">
+    <script>
+        NProgress.configure({
+            showSpinner: false
+        });
+        NProgress.start();
+    </script>
+
+    <div class="mobile-sticky-body-overlay"></div>
+
+    <div class="wrapper">
+
+        {{-- @include('vendor_panel.layouts.nav') --}}
+
+
+
+        <div class="page-wrapper" style="padding-left: 0px !important">
+            <!-- Header -->
+            <header class="main-header " id="header" style="padding: 0px">
+                <nav class="navbar navbar-static-top navbar-expand-lg">
+                    <!-- Sidebar toggle button -->
+                    {{-- <button id="sidebar-toggler" class="sidebar-toggle">
+                        <span class="sr-only">Toggle navigation</span>
+                    </button> --}}
+
+                    <div class="navbar-left ml-4">
+                        <img class="img-fluid" src="{{ asset('vendor_panel/logo.png') }}" alt="" width="92px" height="48px"
+                            srcset="">
+                    </div>
+
+                    {{-- <div class="navbar-right ml-auto">
+                        <a href="/vendor-login" class="btn btn-success" style="padding: 12px 25px;"> Vendor Login
+                        </a>
+                    </div> --}}
+                    <div class="navbar-right ml-auto">
+                        <a href="/vendor-register" class="btn btn-success" style="padding: 12px 25px;"> Register
+                        </a>
+                    </div>
+                </nav>
+
+
+            </header>
+
+            @if(Session::has('success'))
+                <div class="alert alert-success alert-dismissible" role="alert">
+                    <button type="button" class="close" data-dismiss="alert">
+                        <i class="fa fa-times"></i>
+                    </button>
+                    <strong>Success !</strong> {{ session('success') }}
+                </div>
+            @endif 
+
+            @if(Session::has('failure'))
+                <div class="alert alert-danger alert-dismissible" role="alert">
+                    <button type="button" class="close" data-dismiss="alert">
+                        <i class="fa fa-times"></i>
+                    </button>
+                    <strong>Error !</strong> {{ session('failure') }}
+                </div>
+            @endif 
+            @if ($errors->any())
+            @foreach ($errors->all() as $item)
+                <div class="alert alert-danger alert-dismissible" role="alert">
+                    <button type="button" class="close" data-dismiss="alert">
+                        <i class="fa fa-times"></i>
+                    </button>
+                    <strong>Error !</strong> {{ $item }}
+                </div>
+            @endforeach
+            @endif
+            <div class="content-wrapper">
+                <div class="content">
+                    <div class="row justify-content-center">
+                        <div class="col-md-6">
+                            <form method="POST" action="{{ route('password.update') }}">
+                                @csrf
+                    
+                                <!-- Password Reset Token -->
+                                <input type="hidden" name="token" value="{{ $request->route('token') }}">
+                    
+                                <!-- Email Address -->
+                                <div class="form-group mt-4">
+                                    <div class="input-group input-group-sm">
+                                        <input type="email" placeholder="Email"
+                                            class="form-control form-control-sm rounded-0 w-100"
+                                            style="border:unset; border-bottom: 2px solid black; font-size:17px;" name="email" value="{{ $request->email }}" required>
+                                        {{-- <div class="input-group-append">
+                                       <span class="input-group-text rounded-0 fa fa-phone"></span>
+                                  </div> --}}
+                                    </div>
+                                </div>
+                    
+                                <!-- Password -->
+                                <div class="form-group">
+                                    <input type="password" name="password" class="form-control mt-4" placeholder="New Password"
+                                        style="border:unset; border-bottom: 2px solid black; font-size:17px; border-radius:0px;" id="id_password">
+                                        <img id="togglePassword" class="img-fluid" src="{{ asset('vendor_panel/eye.png') }}" style="position: absolute; top: 85px; right: 20px; cursor:pointer">
+                                </div>
+                    
+                                <!-- Confirm Password -->
+                                <div class="form-group">
+                                    <input type="password" name="password_confirmation" class="form-control mt-4" placeholder="Confirm Password"
+                                        style="border:unset; border-bottom: 2px solid black; font-size:17px; border-radius:0px;" id="id_password2">
+                                        <img id="togglePassword2" class="img-fluid" src="{{ asset('vendor_panel/eye.png') }}" style="position: absolute; top: 150px; right: 20px; cursor:pointer">
+                                </div>
+                    
+                                <button  type="submit" class="btn btn-success mt-4 logout-profile">Reset Password</button>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+
+
+
+
             </div>
 
-            <!-- Password -->
-            <div class="mt-4">
-                <x-label for="password" :value="__('Password')" />
 
-                <x-input id="password" class="block mt-1 w-full" type="password" name="password" required />
-            </div>
 
-            <!-- Confirm Password -->
-            <div class="mt-4">
-                <x-label for="password_confirmation" :value="__('Confirm Password')" />
+            {{-- <footer class="footer mt-auto">
+                <div class="row justify-content-center">
+                    <div class="col-md-4">
+                         <a href="/"><i class="mdi mdi-home"></i></a>
+                         <a href="/"><i class="mdi mdi-bell-outline"></i></a>
+                         <a href="/profile"><i class="mdi mdi-account-edit"></i></a>
+                    </div>
+                </div>
+            </footer> --}}
 
-                <x-input id="password_confirmation" class="block mt-1 w-full"
-                                    type="password"
-                                    name="password_confirmation" required />
-            </div>
+        </div>
+    </div>
 
-            <div class="flex items-center justify-end mt-4">
-                <x-button>
-                    {{ __('Reset Password') }}
-                </x-button>
-            </div>
-        </form>
-    </x-auth-card>
-</x-guest-layout>
+
+    <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDCn8TFXGg17HAUcNpkwtxxyT9Io9B_NcM" defer></script>
+    <script src="{{ asset('vendor_panel/assets/plugins/jquery/jquery.min.js') }}"></script>
+    <script src="{{ asset('vendor_panel/assets/plugins/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+    <script src="{{ asset('vendor_panel/assets/plugins/toaster/toastr.min.js') }}"></script>
+    <script src="{{ asset('vendor_panel/assets/plugins/slimscrollbar/jquery.slimscroll.min.js') }}"></script>
+    <script src="{{ asset('vendor_panel/assets/plugins/charts/Chart.min.js') }}"></script>
+    <script src="{{ asset('vendor_panel/assets/plugins/ladda/spin.min.js') }}"></script>
+    <script src="{{ asset('vendor_panel/assets/plugins/ladda/ladda.min.js') }}"></script>
+    <script src="{{ asset('vendor_panel/assets/plugins/jquery-mask-input/jquery.mask.min.js') }}"></script>
+    <script src="{{ asset('vendor_panel/assets/plugins/select2/js/select2.min.js') }}"></script>
+    <script src="{{ asset('vendor_panel/assets/plugins/jvectormap/jquery-jvectormap-2.0.3.min.js') }}"></script>
+    <script src="{{ asset('vendor_panel/assets/plugins/jvectormap/jquery-jvectormap-world-mill.js') }}"></script>
+    <script src="{{ asset('vendor_panel/assets/plugins/daterangepicker/moment.min.js') }}"></script>
+    <script src="{{ asset('vendor_panel/assets/plugins/daterangepicker/daterangepicker.js') }}"></script>
+    <script src="{{ asset('vendor_panel/assets/plugins/jekyll-search.min.js') }}"></script>
+    <script src="{{ asset('vendor_panel/assets/js/sleek.js') }}"></script>
+    <script src="{{ asset('vendor_panel/assets/js/chart.js') }}"></script>
+    <script src="{{ asset('vendor_panel/assets/js/date-range.js') }}"></script>
+    <script src="{{ asset('vendor_panel/assets/js/map.js') }}"></script>
+    <script src="{{ asset('vendor_panel/assets/js/custom.js') }}"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/intl-tel-input/12.1.6/js/intlTelInput.min.js"></script>
+
+    <script>
+        const togglePassword2 = document.querySelector('#togglePassword2');
+        const password2 = document.querySelector('#id_password2');
+
+        togglePassword2.addEventListener('click', function (e) {
+            // toggle the type attribute
+            const type = password2.getAttribute('type') === 'password' ? 'text' : 'password';
+            password2.setAttribute('type', type);
+            // toggle the eye slash icon
+            this.classList.toggle('fa-eye-slash');
+        });
+    </script>
+
+    <script>
+        const togglePassword = document.querySelector('#togglePassword');
+        const password = document.querySelector('#id_password');
+
+        togglePassword.addEventListener('click', function (e) {
+            // toggle the type attribute
+            const type = password.getAttribute('type') === 'password' ? 'text' : 'password';
+            password.setAttribute('type', type);
+            // toggle the eye slash icon
+            this.classList.toggle('fa-eye-slash');
+        });
+    </script>
+
+
+</body>
+
+</html>
+
