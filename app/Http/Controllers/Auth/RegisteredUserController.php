d89c1d6fdf200216c0c40c17417380e35de0cb25
sheikhnayan <sheikhnayan1997@gmail.com>
2023-08-12 01:45:54 +0600

    api change

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -182,7 +182,7 @@ class RegisteredUserController extends Controller
         Auth::login($user);
 
         $accountSid = 'ACddfbd0e90ee11c51c3aa02171f7737d4';
-        $authToken = 'eebbfafabbe35a5554ac772148af643e';
+        $authToken = '21124f1c5033ca54bb61a96e747e5793';
         $twilioNumber = '+14846737439';
         $lineBreak = "\n\n";
         $message = 'Your OTP is '.$otp.' .';
