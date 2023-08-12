655cb6a6a5bf00c3263cd77ae872631951ba05fc
sheikhnayan <sheikhnayan1997@gmail.com>
2023-08-13 01:29:14 +0600

    api change

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -142,7 +142,7 @@ class RegisteredUserController extends Controller
                     ]
                 );
     
-            return (RouteServiceProvider::OTP)->with('success','OTP is sent to '.Auth::user()->phone.'.');
+            return redirect(RouteServiceProvider::OTP)->with('success','OTP is sent to '.Auth::user()->phone.'.');
             
             } catch (TwilioException $e) {
                 return back()->with('failur',$e);
