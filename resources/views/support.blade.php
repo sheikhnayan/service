4923d76683a3a36a9dde9152b29fb231efbb95f1
sheikhnayan <sheikhnayan1997@gmail.com>
2023-08-23 03:47:02 +0600

    test

--- resources/views/support.blade.php
+++ resources/views/support.blade.php
@@ -6,9 +6,9 @@
 
 @section('content')
     <div class="row justify-content-center">
-        <h3 class="text-dark mt-4 text-center">Apply for your new startup company</h3>
         <div class="col-md-8" style="margin-top: 3rem;">
-            <form action="{{ route('support') }}" method="post">
+            <h3 class="text-dark mt-4 text-center mb-4 pb-4">Support & Feedback</h3>
+            <form action="{{ route('support') }}" method="post" class="mt-4 ">
                 @csrf
                 <select name="issue" class="form-control"
                     style="border-radius: 10px; background: #e0e0e0; font-weight: bold;" required>
