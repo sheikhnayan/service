389598b78a5ddab4f6880c88ad726069fe9284fe
sheikhnayan <sheikhnayan1997@gmail.com>
2023-08-23 19:21:53 +0600

    test

--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -7,6 +7,31 @@
 @section('content')
     <div class="row p-4">
         <div class="col-md-12 mb-4">
+            <h3 class="text-dark text-left"> Find Services </h3> 
+        </div>
+        <div class="col-md-1">
+            <a class="text-dark" href="{{ route('user.service-category',[0]) }}">
+                <div class="card" style="width: 70px; border-radius: 12px;">
+                    <img class="img-fluid p-2" src="{{ asset('user/category.png') }}" width="67px" height="67px">
+
+                </div>
+                <p class="text-center mt-2" style="width: 70px;"> All  </p>
+            </a>
+        </div>
+        @foreach ($service as $item)
+        <div class="col-md-1">
+            <a class="text-dark" href="{{ route('user.service-category',[$item->id]) }}">
+                <div class="card" style="width: 70px; border-radius: 12px;">
+                    <img class="img-fluid p-2" src="{{ asset('storage/'.$item->image) }}" width="67px" height="67px">
+                </div>
+                <p class="text-center mt-2" style="width: 70px;">  {{ $item->name }} </p>
+            </a>
+        </div>
+        @endforeach
+    </div>
+    
+    <div class="row p-4">
+        <div class="col-md-12 mb-4">
             <h3 class="text-dark text-left"> Product Categories </h3> 
         </div>
         <div class="col-md-1">
@@ -53,32 +78,8 @@
         @endforeach
     </div>
 
-    <div class="row p-4">
-        <div class="col-md-12 mb-4">
-            <h3 class="text-dark text-left"> Find Services </h3> 
-        </div>
-        <div class="col-md-1">
-            <a class="text-dark" href="{{ route('user.service-category',[0]) }}">
-                <div class="card" style="width: 70px; border-radius: 12px;">
-                    <img class="img-fluid p-2" src="{{ asset('user/category.png') }}" width="67px" height="67px">
 
-                </div>
-                <p class="text-center mt-2" style="width: 70px;"> All  </p>
-            </a>
-        </div>
-        @foreach ($service as $item)
-        <div class="col-md-1">
-            <a class="text-dark" href="{{ route('user.service-category',[$item->id]) }}">
-                <div class="card" style="width: 70px; border-radius: 12px;">
-                    <img class="img-fluid p-2" src="{{ asset('storage/'.$item->image) }}" width="67px" height="67px">
-                </div>
-                <p class="text-center mt-2" style="width: 70px;">  {{ $item->name }} </p>
-            </a>
-        </div>
-        @endforeach
-    </div>
-
-    <div class="row p-4">
+    {{-- <div class="row p-4">
         <div class="col-md-12 mb-4">
             <h3 class="text-dark text-left"> Latest Events </h3> 
         </div>
@@ -89,12 +90,11 @@
                         <img class="img-fluid" src="{{ asset('storage/'.$item->image) }}" style="margin: auto; padding: 5px;">
                         <p style="padding: 5px; margin:auto; color:#fff">{{ $item->name }}</p>
                         <div class="button text-left">
-                        {{-- <a href="{{ route('user.service.edit',[$item->id]) }}" style="color:#8f94a1" class="btn btn-transparent"> <span class="mdi mdi-table-edit"></span> Edit</a> --}}
                         <a href="{{ route('user.event.show',[$item->id]) }}" class="btn btn-transparent" style="color:#fff; font-size:1rem"> <span class="mdi mdi-eye"></span> View</a>
                         </div>
                     </div>
                 </div>
             @endif
         @endforeach
-    </div>
+    </div> --}}
 @endsection
