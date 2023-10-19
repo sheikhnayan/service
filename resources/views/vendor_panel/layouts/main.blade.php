cb5c543bb5d1aa17b417f27fe4a52fc19d1a081a
sheikhnayan <sheikhnayan1997@gmail.com>
2023-10-19 20:55:19 +0600

    update

--- resources/views/vendor_panel/layouts/main.blade.php
+++ resources/views/vendor_panel/layouts/main.blade.php
@@ -130,13 +130,13 @@
 
                     @endif
 
-                    @if (Auth::user()->type == 'vendor' | Auth::user()->type == 'user')
+                    @if (Auth::user()->type == 'user')
 
                     <div class="navbar-right ml-auto">
-                        <img class="img-fluid" src="{{ asset('storage'.Auth::user()->vendor->logo) }}" style="border-radius: 50%; width: 50px; height: 50px"
-                            srcset="">
+                        {{-- <img class="img-fluid" src="{{ asset('storage'.Auth::user()->vendor->logo) }}" style="border-radius: 50%; width: 50px; height: 50px"
+                            srcset=""> --}}
                         <span class=" text-dark " style="font-weight:bold; font-size: 1rem">
-                            {{ Auth::user()->vendor->company_name }}
+                            {{ Auth::user()->name }}
                         </span>
                         <span class="mdi mdi-arrow-down-drop-circle drop-down"></span>
                         <div class="dropdown-menu">
@@ -146,6 +146,27 @@
                             @else
                             <a class="dropdown-item" href="{{ route('user.wish') }}">WishList</a>
                             @endif
+                            <form action="{{ route('logout') }}" method="post">
+                            @csrf
+                            <button type="submit" class="dropdown-item" href="#">Logout</button>
+                            </form>
+                        </div>
+                    </div>
+                    
+
+                    @endif
+
+                    @if (Auth::user()->type == 'admin')
+
+                    <div class="navbar-right ml-auto">
+                        {{-- <img class="img-fluid" src="{{ asset('storage'.Auth::user()->vendor->logo) }}" style="border-radius: 50%; width: 50px; height: 50px"
+                            srcset=""> --}}
+                        <span class=" text-dark " style="font-weight:bold; font-size: 1rem">
+                            {{ Auth::user()->name }}
+                        </span>
+                        <span class="mdi mdi-arrow-down-drop-circle drop-down"></span>
+                        <div class="dropdown-menu">
+                            <a class="dropdown-item" href="{{ route('profile') }}">Profile</a>
                             <form action="{{ route('logout') }}" method="post">
                             @csrf
                             <button type="submit" class="dropdown-item" href="#">Logout</button>
