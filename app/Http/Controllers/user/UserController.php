5a19f1f97e1af5dfac42a953641197bf10e72fc5
sheikhnayan <sheikhnayan1997@gmail.com>
2023-11-26 10:23:57 +0600

    a

--- app/Http/Controllers/user/UserController.php
+++ app/Http/Controllers/user/UserController.php
@@ -147,7 +147,7 @@ class UserController extends Controller
                 $distance = GeoFacade::setPoint([$lat,$lon])
                     ->setOptions(['units' => ['km']])
                     // you can set unlimited lat/long points.
-                    ->setPoint([(string)$value->vendor->vendor->address_latitude,(string)$value->vendor->vendor->address_longitude])
+                    ->setPoint([(float)$value->vendor->vendor->address_latitude,(float)$value->vendor->vendor->address_longitude])
                     // get the calculated distance between each point
                     ->getDistance();
 
