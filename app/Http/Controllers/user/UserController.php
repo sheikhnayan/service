af8f51f38692868ae342098bb57045060573e323
sheikhnayan <sheikhnayan1997@gmail.com>
2023-11-26 08:57:14 +0600

    a

--- app/Http/Controllers/user/UserController.php
+++ app/Http/Controllers/user/UserController.php
@@ -153,7 +153,7 @@ class UserController extends Controller
 
                     if ($value->vendor->id == 113) {
                         # code...
-                        dd($lon);
+                        dd($value->vendor->vendor->address_longitude);
                     }
 
                 if ($distance['1-2']['km'] <= 10) {
